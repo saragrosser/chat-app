18cbe12254c71b78841d82af74e2793de93b4024
saragrosser <sgrosser00@gmail.com>
2024-05-29 19:27:24 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,42 @@
+## Project description
+
+A mobile chat application built with React Native. The app will provide users with a chat interface and options to share images and their location.
+
+### Getting Started
+
+To run this app locally, you'll need to follow these steps:
+
+- Clone this repository.
+- Set up Expo in your development environment:
+
+  - Install Expo and Expo CLI, as this is the platform you’ll use to build your app;
+
+    npm install -g expo-cli
+
+  - Install Expo Go app on your mobile device, so that you can test your app on your own mobile device;
+
+    Search for the Expo Go app in the relevant app store for your device (iOS or Android)
+
+  - Create an Expo account.
+
+### Prerequisites
+
+Before installing Expo, ensure you have a suitable version of Node installed. At the time of writing, Expo only supports Node 16.. at max.
+
+Node.js: Download and install Node.js. For this you can use the nvm tool https://github.com/nvm-sh/nvm
+
+    nvm install 16.19.0
+    nvm use 16.19.0
+    nvm alias default 16.19.0
+
+Navigate to the chat-app directory and run
+
+    npm install
+
+## Start the App
+
+- Use the Expo Go App on your mobile device to check the UI
+
+- Initialize the app in your terminal:
+
+      npx expo start
---
+++ components/start.js
@@ -0,0 +1,177 @@
+import { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  ImageBackground,
+  TouchableOpacity,
+  Platform,
+  KeyboardAvoidingView,
+} from "react-native";
+
+// Define the Start component
+const Start = ({ navigation }) => {
+  // State to hold the name input value
+  const [name, setName] = useState("");
+  // State to hold the chosen background color
+  const [background, setBackground] = useState("");
+
+  return (
+    <ImageBackground
+      source={require("../img/background-img.png")}
+      style={styles.imageBackground}
+      resizeMode="cover"
+    >
+      <Text style={styles.appTitle}>APP Title</Text>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.textInput}
+          value={name}
+          onChangeText={setName}
+          placeholder="Your Name"
+        ></TextInput>
+
+        <View style={styles.chooseColorBox}>
+          <Text style={styles.chooseColorText}>Choose Background Color:</Text>
+          <View style={styles.colorButtonsContainer}>
+            {/* Render a TouchableOpacity for each color option */}
+            <TouchableOpacity
+              accessible={true}
+              accessibilityLabel="More options"
+              accessibilityHint="Lets you choose to send an image or your geolocation."
+              accessibilityRole="button"
+              style={[
+                styles.chooseColor,
+                { backgroundColor: "#090C08" },
+                background === "#090C08" && styles.selectedColor,
+              ]}
+              // Set the function to handle button press
+              onPress={() => setBackground("#090C08")}
+            ></TouchableOpacity>
+            <TouchableOpacity
+              style={[
+                styles.chooseColor,
+                { backgroundColor: "#474056" },
+                background === "#474056" && styles.selectedColor,
+              ]}
+              onPress={() => setBackground("#474056")}
+            ></TouchableOpacity>
+            <TouchableOpacity
+              style={[
+                styles.chooseColor,
+                { backgroundColor: "#8A95A5" },
+                background === "#8A95A5" && styles.selectedColor,
+              ]}
+              onPress={() => setBackground("#8A95A5")}
+            ></TouchableOpacity>
+            <TouchableOpacity
+              style={[
+                styles.chooseColor,
+                { backgroundColor: "#B9C6AE" },
+                background === "#B9C6AE" && styles.selectedColor,
+              ]}
+              onPress={() => setBackground("#B9C6AE")}
+            ></TouchableOpacity>
+          </View>
+        </View>
+
+        {/* Render a TouchableOpacity for starting the chat */}
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() =>
+            navigation.navigate("Chat", { name: name, background: background })
+          }
+        >
+          <Text style={styles.textButton}>Start Chatting</Text>
+        </TouchableOpacity>
+      </View>
+      {Platform.OS === "android" ? (
+        <KeyboardAvoidingView behavior="height" />
+      ) : null}
+    </ImageBackground>
+  );
+};
+
+// Define styles for the component
+const styles = StyleSheet.create({
+  imageBackground: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  appTitle: {
+    flex: 1,
+    fontSize: 45,
+    fontWeight: "600",
+    color: "#ffffff",
+    justifyContent: "center",
+    marginTop: 80,
+  },
+  container: {
+    width: "88%",
+    height: "44%",
+    backgroundColor: "white",
+    alignItems: "center",
+    marginBottom: 30,
+    justifyContent: "space-evenly",
+  },
+
+  textInput: {
+    width: "88%",
+    padding: 15,
+    borderWidth: 1,
+    fontSize: 16,
+    fontWeight: "300",
+    color: "#757083",
+    opacity: 0.5,
+    borderColor: "#757083",
+  },
+  button: {
+    width: "88%",
+    height: "20%",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#757083",
+    padding: 10,
+  },
+  textButton: {
+    fontSize: 16,
+    fontWeight: "600",
+    color: "#ffffff",
+  },
+  chooseColorBox: {
+    width: "88%",
+    height: "20%",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  colorButtonsContainer: {
+    flexDirection: "row",
+    alignSelf: "flex-start",
+    justifyContent: "space-between",
+  },
+  chooseColor: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    border: 3,
+    marginRight: 15,
+    borderColor: "white",
+  },
+  selectedColor: {
+    borderColor: "#FCD95B",
+    borderWidth: 3,
+  },
+
+  chooseColorText: {
+    flex: 1,
+    fontSize: 16,
+    fontWeight: "300",
+    color: "#757083",
+    textAlign: "left",
+    alignSelf: "flex-start",
+  },
+});
+
+export default Start;
